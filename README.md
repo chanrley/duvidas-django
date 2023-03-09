38ae2e0a881da2f20fff05801c03e7cb0529c1e2
Chanrley Tadeu Goncalves Ferreira <chanrley_tadeu_g_ferreira@internal.carrefour.com>
2023-03-09 14:42:07 -0300

    Credenciais de banco adicionadas via arquivo

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Esse projeto foi baseado numa simples aplicação de market place usando o Django.
 
-Esse repositório é parte de um vídeo tutorial criado pela FreeCodeCamp.
+Esse repositório é parte de um vídeo tutorial criado pela FreeCodeCamp com customizações para atender área DVA(Divisão de Vendas e Atendimento)
 
 ## Author
 Esse repositório foi criado pela equipe DVA.
--- puddle/settings.py
+++ puddle/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 #Maneira correta com arquivos de configuração
-# import environ
-# env = environ.Env()
-# environ.Env.read_env()
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = env("SECRET_KEY")
-SECRET_KEY = 'django-insecure-j4ippt+3h39u4ontllpc8a(4h&^god(7aicz#@q^sl_(w)2otp'
+SECRET_KEY = env("SECRET_KEY")
+# SECRET_KEY = 'django-insecure-j4ippt+3h39u4ontllpc8a(4h&^god(7aicz#@q^sl_(w)2otp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -93,6 +93,7 @@ WSGI_APPLICATION = 'puddle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -101,7 +102,7 @@ DATABASES = {
 }
 
 
-"""
+
 # Maneira hardcoded
 DATABASES = {
     'default': {
@@ -115,7 +116,7 @@ DATABASES = {
 }
 """
 
-"""
+
 # maneira correta
 # Your secret key
 #SECRET_KEY = env("SECRET_KEY")
@@ -130,7 +131,6 @@ DATABASES = {
         'PORT': env("DB_PORT"),
     }
 }
-"""
 
 
 # Password validation
