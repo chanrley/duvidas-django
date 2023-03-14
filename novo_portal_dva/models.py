1ab8383374c4e40515f65dab902a795fab544d57
Chanrley Tadeu Goncalves Ferreira <chanrley_tadeu_g_ferreira@internal.carrefour.com>
2023-03-14 12:11:24 -0300

    Antes da criação dos CRUDS de usuários

--- novo_portal_dva/models.py
+++ novo_portal_dva/models.py
@@ -4,7 +4,7 @@ class Menu(models.Model):
     nome = models.CharField(max_length=255)
 
     class Meta:
-        ordering = ('id', 'nome',)
+        ordering = ('nome',)
         verbose_name_plural = 'Menus'
     
     def __str__(self):
@@ -16,9 +16,25 @@ class SubMenu(models.Model):
     menu = models.ForeignKey(to=Menu, on_delete=models.CASCADE)
     
     class Meta:
-        ordering = ('menu',)
+        ordering = ('nome', 'menu',)
         verbose_name_plural = 'SubMenus'
 
     def __str__(self):
         return f"{self.nome} - {self.menu}"
 
+# class Usuario(models.Model):
+#     drt = models.CharField(max_length=11)
+#     nome = models.CharField(max_length=100)
+#     cargo = models.CharField(max_length=100)
+#     perfil_acesso = models.ForeignKey(to=GrupoAcesso, on_delete=models.CASCADE)
+#     class Meta:
+#         ordering = ('drt',)
+#         verbose_name_plural = 'Usuários'
+
+# class GrupoAcesso(models.Model):
+#     usuario_id = models.ForeignKey(to=Usuario, on_delete=models.CASCADE)
+#     grupo_acesso = models.IntegerField()
+    
+#     class Meta:
+#         ordering = ('drt',)
+#         verbose_name_plural = 'Usuários'
